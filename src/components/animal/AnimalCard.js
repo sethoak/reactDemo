5d07a1f5b3653f8975e9b5c728b3c3de8faff7c6
sethoak <sethoak@gmail.com>
2019-10-04 14:52:33 -0500

    updated animal to pull from DB w/CSS

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./animal.css";
 
 class AnimalCard extends Component {
   render() {
@@ -8,10 +9,10 @@ class AnimalCard extends Component {
           <picture>
             <img src={require("./dog.svg")} alt="My Dog" />
           </picture>
-          <h3>
-            Name: <span className="card-petname">Doodles</span>
-          </h3>
-          <p>Breed: Poodle</p>
+          <h2>
+            Name: <span className="card-petname">{this.props.animal.name}</span>
+          </h2>
+          <p>Breed: {this.props.animal.breed}</p>
         </div>
       </div>
     );
