5b6fe60c573151bda78145aceadea91cdad7dff7
sethoak <sethoak@gmail.com>
2019-10-04 12:14:29 -0500

    working on exercises

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 import Home from "./home/Home";
 import AnimalList from "./animal/AnimalList";
 //only include these once they are built - previous practice exercise
-import LocationCard from "./location/LocationCard";
-import EmployeeCard from "./employee/EmployeeCard";
+import LocationList from "./location/LocationList";
+import EmployeeList from "./employee/EmployeeList";
 import OwnerCard from "./owner/OwnerCard";
 
 class ApplicationViews extends Component {
@@ -27,13 +27,13 @@ class ApplicationViews extends Component {
         <Route
           path="/locations"
           render={props => {
-            return <LocationCard />;
+            return <LocationList />;
           }}
         />
         <Route
           path="/employees"
           render={props => {
-            return <EmployeeCard />;
+            return <EmployeeList />;
           }}
         />
         <Route
--- src/modules/AnimalManager.js
+++ src/modules/AnimalManager.js
@@ -1,10 +1,19 @@
 const remoteURL = "http://localhost:5002";
 
 export default {
-  getAnimal(id) {
+  get(id) {
     return fetch(`${remoteURL}/animals/${id}`).then(result => result.json());
   },
   getAll() {
     return fetch(`${remoteURL}/animals`).then(result => result.json());
+  },
+  getAllLocation() {
+    return fetch(`${remoteURL}/locations`).then(result => result.json());
+  },
+  getAllEmployee() {
+    return fetch(`${remoteURL}/employees`).then(result => result.json());
+  },
+  getAllOwner() {
+    return fetch(`${remoteURL}/owners`).then(result => result.json());
   }
 };
